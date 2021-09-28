38c128e90d4c0d0dbc9870e1d273d859d3abba72
coryax <91561152+coryax@users.noreply.github.com>
2021-09-28 13:45:53 -0700

    Add files via upload

---
+++ athletex-periphery/test/RouterEventEmitter.sol
@@ -0,0 +1,97 @@
+pragma solidity =0.6.6;
+
+import '../interfaces/IAthleteXRouter.sol';
+
+contract RouterEventEmitter {
+    event Amounts(uint[] amounts);
+
+    receive() external payable {}
+
+    function swapExactTokensForTokens(
+        address router,
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IAthleteXRouter(router).swapExactTokensForTokens.selector, amountIn, amountOutMin, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapTokensForExactTokens(
+        address router,
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IAthleteXRouter(router).swapTokensForExactTokens.selector, amountOut, amountInMax, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapExactAVAXForTokens(
+        address router,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IAthleteXRouter(router).swapExactAVAXForTokens.selector, amountOutMin, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapTokensForExactAVAX(
+        address router,
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IAthleteXRouter(router).swapTokensForExactAVAX.selector, amountOut, amountInMax, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapExactTokensForAVAX(
+        address router,
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IAthleteXRouter(router).swapExactTokensForAVAX.selector, amountIn, amountOutMin, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapAVAXForExactTokens(
+        address router,
+        uint amountOut,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IAthleteXRouter(router).swapAVAXForExactTokens.selector, amountOut, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+}
